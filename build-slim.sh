8f7842b8ab47953bb58aeeb86613b38d05767c9d
Michael Hart <michael.hart.au@gmail.com>
2020-03-12 10:09:32 -0400

    v13.11.0 full and slim builds

--- build-slim.sh
+++ build-slim.sh
@@ -1,8 +1,8 @@
 #/bin/sh
 
 docker build --squash \
-  -t mhart/alpine-node:slim-13.10.1 \
-  -t mhart/alpine-node:slim-13.10 \
+  -t mhart/alpine-node:slim-13.11.0 \
+  -t mhart/alpine-node:slim-13.11 \
   -t mhart/alpine-node:slim-13 \
   -t mhart/alpine-node:slim \
   -f slim.dockerfile .
--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #/bin/sh
 
 docker build --pull --squash \
-  -t mhart/alpine-node:13.10.1 \
-  -t mhart/alpine-node:13.10 \
+  -t mhart/alpine-node:13.11.0 \
+  -t mhart/alpine-node:13.11 \
   -t mhart/alpine-node:13 \
   -t mhart/alpine-node:latest \
   -f extract.dockerfile \
