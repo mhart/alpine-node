1ba77a177e4fd974cfcddf7842388eb5cea770b1
Michael Hart <michael.hart.au@gmail.com>
2020-06-02 15:24:19 -0400

    v10.21.0 full and slim builds

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #/bin/sh
 
 docker build --pull --squash \
-  -t mhart/alpine-node:12.18.0 \
-  -t mhart/alpine-node:12.18 \
-  -t mhart/alpine-node:12 \
+  -t mhart/alpine-node:10.21.0 \
+  -t mhart/alpine-node:10.21 \
+  -t mhart/alpine-node:10 \
   -f build.dockerfile \
   .
--- publish.sh
+++ publish.sh
@@ -1,6 +1,6 @@
 #/bin/sh
 
-for tag in 12.18.0 12.18 12 slim-12.18.0 slim-12.18 slim-12; do
+for tag in 10.21.0 10.21 10 slim-10.21.0 slim-10.21 slim-10; do
   git tag -f $tag
   git push -f origin $tag
   docker push mhart/alpine-node:$tag
