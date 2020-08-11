e3d07a373e2a9dbb1fb2c78a8c1a73cb4be54253
Michael Hart <michael.hart.au@gmail.com>
2020-08-11 13:21:03 -0400

    v14.8.0 full and slim builds

--- build-slim.sh
+++ build-slim.sh
@@ -1,8 +1,8 @@
 #/bin/sh
 
 docker build --squash \
-  -t mhart/alpine-node:slim-14.7.0 \
-  -t mhart/alpine-node:slim-14.7 \
+  -t mhart/alpine-node:slim-14.8.0 \
+  -t mhart/alpine-node:slim-14.8 \
   -t mhart/alpine-node:slim-14 \
   -t mhart/alpine-node:slim \
   -f slim.dockerfile .
--- publish.sh
+++ publish.sh
@@ -1,6 +1,6 @@
 #/bin/sh
 
-for tag in 14.7.0 14.7 14 slim-14.7.0 slim-14.7 slim-14 latest slim; do
+for tag in 14.8.0 14.8 14 slim-14.8.0 slim-14.8 slim-14 latest slim; do
   git tag -f $tag
   git push -f origin $tag
   docker push mhart/alpine-node:$tag
