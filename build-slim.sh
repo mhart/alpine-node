630f7b36aa626c3e16e4fca27b80cf3fe03e1e99
Michael Hart <michael.hart.au@gmail.com>
2020-11-04 17:30:25 -0500

    v15.1.0 full and slim builds

--- build-slim.sh
+++ build-slim.sh
@@ -1,8 +1,8 @@
 #/bin/sh
 
 docker build --squash \
-  -t mhart/alpine-node:slim-15.0.1 \
-  -t mhart/alpine-node:slim-15.0 \
+  -t mhart/alpine-node:slim-15.1.0 \
+  -t mhart/alpine-node:slim-15.1 \
   -t mhart/alpine-node:slim-15 \
   -t mhart/alpine-node:slim \
   -f slim.dockerfile .
